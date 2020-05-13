8a5ca655732ca2197b5dd817d288a9a06bbb4171
Tangxinming <tangxinming@csii.com.cn>
2020-05-13 10:51:16 +0800

    update

--- docs/.vuepress/config/headerConfig.js
+++ docs/.vuepress/config/headerConfig.js
@@ -1,13 +1,11 @@
 module.exports = [
   ['link', { rel: 'icon', href: '/favicon.ico' }],
   ['meta', { name: 'author', content: 'bright' }],
-  ['meta', { name: 'keywords', content: 'vuepress introduce' }],
+  ['meta', { name: 'keywords', content: '前端 随记 笔记' }],
   ['link', { rel: 'manifest', href: '/manifest.json' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
   ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-  ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
 ]
